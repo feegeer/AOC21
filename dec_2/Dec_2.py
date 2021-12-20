d516b3bf73584174c72e8fefc418af6777544d45
feegeer <11feem1bif@hft-stutgart.de>
2021-12-20 15:06:16 +0100

    removed some redundancy

--- dec_2/Dec_2.py
+++ dec_2/Dec_2.py
@@ -2,19 +2,18 @@ import re
 
 data = open('C:\\Users\\emmaf\\PycharmProjects\\AOC21\\dec_2\\dec_2_data', 'r').read().split('\n')
 
+forward_pattern = re.compile('forward [0-9]+')
+up_pattern = re.compile("up [0-9]+")
+down_pattern = re.compile("down [0-9]+")
+
 
 def get_position_part1():
     forward_pos = 0
     depth = 0
 
-    forward_pattern = re.compile('forward [0-9]+')
-    up_pattern = re.compile("up [0-9]+")
-    down_pattern = re.compile("down [0-9]+")
-
     for command in data:
 
         motion = int((command.split(' ')[1]))
-
         if forward_pattern.match(command):
             forward_pos += motion
         elif up_pattern.match(command):
@@ -32,10 +31,6 @@ def get_position_part2():
     forward_pos = 0
     depth = 0
     aim = 0
-
-    forward_pattern = re.compile('forward [0-9]+')
-    up_pattern = re.compile("up [0-9]+")
-    down_pattern = re.compile("down [0-9]+")
 
     for command in data:
 
@@ -55,5 +50,5 @@ def get_position_part2():
     return forward_pos * depth
 
 
-# print(get_position_part1())
+print(get_position_part1())
 print(get_position_part2())
